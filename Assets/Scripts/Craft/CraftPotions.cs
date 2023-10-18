9970bdf435268f30df2a6326ed171b06c15426c3
Júlia <julia.martos@enti.cat>
2023-10-18 12:27:00 +0200

    Crafteo potis cutrillo, pero acabadillo

--- Assets/Scripts/Craft/CraftPotions.cs
+++ Assets/Scripts/Craft/CraftPotions.cs
@@ -6,13 +6,26 @@ public class CraftPotions : MonoBehaviour
 {
     public GameObject handFlower1;
     public GameObject handFlower4;
+    public GameObject newPotion;
+    
+
+    List<Potions> potions = new List<Potions>();
+    int potionCounter = 0;
 
     public void OnMouseDown()
     {
-      if(handFlower1.activeSelf == true || handFlower4.activeSelf == true)
+      if(handFlower1.activeSelf == true && handFlower4.activeSelf == true)
         {
-            Debug.Log("true");
+            handFlower1.SetActive(false);
+            handFlower4.SetActive(false);
+
+            //potions.Add(newPotion.gameObject.GetComponent<Potions>().GetPotion());
+            potionCounter++;
+            Debug.Log(potionCounter);
+
+            newPotion.SetActive(true);
         }
     }
 
+    public List<Potions> GetPotions() { return potions; }
 }
